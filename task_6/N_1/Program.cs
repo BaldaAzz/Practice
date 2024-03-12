0f1e1220c6441fcabce2e22f4bf6ae1b4e8fcc5b
BaldaAzz <kill200419@gmail.com>
2024-03-12 14:20:21 +0300

    Added task_6/N_1

--- task_6/N_1/Program.cs
+++ task_6/N_1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace N_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine(); 
+
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine("Строка палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не палиндромом");
+            }
+        }
+
+        static bool IsPalindrome(string str)
+        {   
+            str = str.ToLower();
+            string newStr = str.Replace(" ", string.Empty);
+            char[] charArray = newStr.ToCharArray();
+            Array.Reverse(charArray);
+            string reverseStr = new string(charArray);
+
+            return newStr == reverseStr;
+        }
+    }
+}
