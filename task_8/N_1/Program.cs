0723b97f0c26fcf284c736ab074bb72298f417c5
BaldaAzz <kill200419@gmail.com>
2024-03-13 14:24:19 +0300

    Added task_8

---
+++ task_8/N_1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace N_1
+{
+    class Program
+    {
+        public struct TRAIN
+        {
+            public string destination;
+            public int trainNumber;
+            public DateTime departureTime;
+        }
+
+        public static void PrintTrainInfo(int trainNumber, TRAIN[] trains)
+        {
+            bool founded = false;
+
+            foreach (TRAIN train in trains)
+            {
+                if (train.trainNumber == trainNumber)
+                {
+                    Console.WriteLine(train.destination);
+                    Console.WriteLine(train.trainNumber);
+                    Console.WriteLine(train.departureTime);
+
+                    founded = true;
+                }
+            }
+
+            if (!founded)
+            {
+                Console.WriteLine("Данный поезд не был найден! ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TRAIN[] trains = new TRAIN[3];
+
+            for (int i = 0; i < trains.Length; i++)
+            {
+                Console.WriteLine("Пункт назначения");
+                trains[i].destination = Console.ReadLine();
+
+                Console.WriteLine("Номер поезда");
+                trains[i].trainNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Дата отправления");
+                trains[i].departureTime = Convert.ToDateTime(Console.ReadLine());
+            }
+
+            trains = trains.OrderBy(i => i.trainNumber).ToArray();
+
+            PrintTrainInfo(Convert.ToInt32(Console.ReadLine()), trains);
+        }
+    }
+}
---
+++ task_8/N_2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace N_2
+{
+    class Program
+    {
+        public enum EmployeePosition
+        {
+            Manager = 160,
+            Developer = 180,
+            Designer = 150,
+            Analyst = 170
+        }
+
+        static void Main(string[] args)
+        {
+            EmployeePosition position = EmployeePosition.Manager;
+
+            Console.WriteLine($"Работник на должности {position} должен отработать {(int)position} часов");
+        }
+    }
+}
