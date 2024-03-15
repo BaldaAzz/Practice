f425a17d8abcd8968e1b5e335ca65989720f5100
BaldaAzz <kill200419@gmail.com>
2024-03-15 13:26:08 +0300

    Added task_10

---
+++ task_10/N_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace N_1
+{
+    class Student
+    {
+        private string name;
+        protected double s;
+
+        public Student(string name, double s)
+        {
+            this.name = name;
+            this.s = s;
+        }
+
+        public decimal Scholarship()
+        {
+            return 300000 + 10000 * (Convert.ToDecimal(this.s) - 5);
+        }
+    }
+
+    class Magistr: Student
+    {   
+        private string speciality;
+
+        public Magistr(string name, double s, string speciality): base(name, s)
+        {
+            this.speciality = speciality;
+        }
+
+        public decimal Scholarship(decimal m)
+        {
+            return 300000 + 10000 * (Convert.ToDecimal(this.s) - 5) + m;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s =  new("Олег", 7.3);
+            Console.WriteLine($"Степеньдия: {s.Scholarship()}");
+
+            Magistr m = new("Другой Олег", 9.1, "биология");
+            Console.WriteLine($"Степеньдия: {m.Scholarship(1000)}");
+        }
+    }
+}
