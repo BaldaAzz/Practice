8f1de84583e95dcba6a8723f634783929b3448cc
BaldaAzz <ruslanfedosvich2019@gmial.com>
2024-04-08 15:19:09 +0300

    Added task_26

---
+++ task_26/N_1/Models/Flower.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace N_1.Models
+{
+    public class Flower
+    {
+        public string FlowerName { get; set; }
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return new StringBuilder()
+                .Append($"Flower is {FlowerName}")
+                .Append($"Flower price is {Price:f4} rub")
+                .ToString();
+        }
+    }
+}
